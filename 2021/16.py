08d8991e62c09e743f5f3b834ad6af11daa58d5e
aiden-perkins <perkinsaidenm@gmail.com>
2023-11-08 00:13:15 -0800

    2021 day 16 p2 & update readme

--- 2021/16.py
+++ 2021/16.py
@@ -1,14 +1,26 @@
+import operator
+import functools
+
 ls = open('./input.txt', 'r').read().split('\n')[:-1][0]
 
 """ Part 1 """
 
-ls = 'C200B40A82'
 bits = ''
 for b in ls:
     bits += str(bin(int(b, 16))[2:].zfill(4))
 
+operations = {
+    0: lambda x: sum([y[2] for y in x]),
+    1: lambda x: functools.reduce(operator.mul, [y[2] for y in x]),
+    2: lambda x: min([y[2] for y in x]),
+    3: lambda x: max([y[2] for y in x]),
+    5: lambda x: 1 if x[0][2] > x[1][2] else 0,
+    6: lambda x: 1 if x[0][2] < x[1][2] else 0,
+    7: lambda x: 1 if x[0][2] == x[1][2] else 0
+}
 
-def parse_multiple_packets(bit_string):
+
+def parse_multiple_packets(bit_string, packets_c):
     packets = []
     while bit_string != '':
         c_i = 6
@@ -18,35 +30,38 @@ def parse_multiple_packets(bit_string):
             ltid = int(bit_string[6:7], 2)
             if ltid == 0:
                 c_i += 15
+                packets_count = 0
                 new_bits = bit_string[22:22 + int(bit_string[7:22], 2)]
             else:
                 c_i += 11
+                packets_count = int(bit_string[7:18], 2)
                 new_bits = bit_string[18:]
-            packets += [[pv, ptid, 'sub', c_i]]
-            new_packets = parse_multiple_packets(new_bits)
+            new_packets = parse_multiple_packets(new_bits, packets_count)
             for n_p in new_packets:
                 c_i += n_p[3]
-            packets += new_packets
+                pv += n_p[0]
+            packets += [[pv, ptid, operations[ptid](new_packets), c_i]]
         else:
             n_b_s = ''
             for bit_group in range(len(bit_string[6:]) // 5):
-                n_b_s += bit_string[6 + (bit_group * 5):((bit_group + 1) * 5) + 6]
+                n_b_s += bit_string[7 + (bit_group * 5):((bit_group + 1) * 5) + 6]
                 if bit_string[6 + (bit_group * 5):7 + (bit_group * 5)] == '0':
                     break
-            c_i += len(n_b_s)
-            packets.append([pv, ptid, int(n_b_s, 2), len(n_b_s) + 6])
+            c_i += (len(n_b_s) // 4) * 5
+            lv = int(n_b_s, 2)
+            packets.append([pv, ptid, lv, ((len(n_b_s) // 4) * 5) + 6])
         bit_string = bit_string[c_i:]
-        if '1' not in bit_string:
+        if '1' not in bit_string or (len(packets) > 0 and len(packets) == packets_c):
             break
     return packets
 
 
 total_version_nums = 0
-ps = parse_multiple_packets(bits)
+ps = parse_multiple_packets(bits, 0)
 for packet in ps:
     total_version_nums += packet[0]
-print(total_version_nums)
+print(ps[0][0])
 
 """ Part 2 """
 
-print(ps)
+print(ps[0][2])
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-AOC
+# Advent of Code
+
+This repository contains my solutions for the [Advent of Code](https://adventofcode.com/) programming challenges.
+My goal is the eventually earn all 50 stars for every year but I am far from that (22/50 for 2022, 34/50 for 2021, & 5/50 for 2020).
+
+___Input:___
+
+- To get an input you first need to get your session token from [the Advent of Code website](https://adventofcode.com/) and put it in the empty string on line 11 in [in.py](in.py).
+- You can either run [.a.bat](.a.bat) (it's called `.a.bat` so I can press `.` on my keyboard, then tab, allowing me to get the input faster) if you are on windows or just `python3 in.py` and then enter the day/year, if you don't put a year it will default to 2021.
+
+
+___Running:___
+
+- All the days require the input.txt to be in the root folder and will not work otherwise, in.py automatically puts it in the correct place so if you used that you can ignore where input.txt is.
+- To run any specific day all you need to do is `python3 [year]/[day].py` and just replace `year` and `day` with whatever year/day you want.
+- Most days will print out the answers for both part 1 & 2 but this varies depending on what the answer actually is.
+
+
+___Other:___
+
+- [t.py](t.py) is just what I use as a template for starting new days.
+- [BinaryHeap.py](2021/BinaryHeap.py) is just the data structure I used for 2021 day 15 so it could run much faster, this file is just from one of my classes and I used it because I was familiar with how it works.
+
