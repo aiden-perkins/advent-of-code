1a6b17e15b98b76cfa95b6f76b5c0a0ffa11e423
aiden-perkins <perkinsaidenm@gmail.com>
2023-12-15 01:39:35 -0800

    oops forgot this

---
+++ 2015/1.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+
+string get_input() {
+    string input;
+    string input_line;
+    ifstream inputFile("./input.txt");
+    if (inputFile.is_open()) {
+        while (getline(inputFile, input_line)) {
+            input += input_line;
+        }
+        inputFile.close();
+    }
+    return input;
+}
+
+
+int part_1(string &input) {
+    int current_level = 0;
+    for (int i = 0; i < input.length(); ++i) {
+        if (input[i] == '(') {
+            current_level++;
+        } else {
+            current_level--;
+        }
+    }
+    return current_level;
+}
+
+
+int part_2(string &input) {
+    int current_level = 0;
+    int pos = 0;
+    for (int i = 0; i < input.length(); ++i) {
+        if (input[i] == '(') {
+            current_level++;
+        } else {
+            current_level--;
+        }
+        if (current_level < 0) {
+            pos = i + 1;
+            break;
+        }
+    }
+    return pos;
+}
+
+
+int main() {
+    string input = get_input();
+    cout << part_1(input) << endl;
+    cout << part_2(input) << endl;
+    return 0;
+}
